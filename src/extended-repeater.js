4909889491205cae4bbb9ac0cde2789abddb9bcb
Shamayana <shamayana777@gmail.com>
2021-09-21 15:02:47 +0300

    feat: solve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,20 +2,30 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  str = String(str);
+  let add = String(options.addition);
+  let sep = options.separator || '+';
+  let addSep = options.additionSeparator || '|';
+  let addTimes = options.additionRepeatTimes || 1;
+  let times = options.repeatTimes || 1;
+
+  if(add == 'undefined') add = '';
+
+  let repeatingAdd = (add + addSep).repeat(addTimes - 1) + add;
+
+  return (str + repeatingAdd + sep).repeat(times - 1) + str + repeatingAdd;
 }
